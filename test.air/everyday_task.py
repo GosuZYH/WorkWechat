67e7ad7cec1e1df9c6bc4d432fd1125acad6c76f
chenyujia_tester <1215793664@qq.com>
2021-11-11 13:35:28 +0800

    feat:添加一些进入聊天页面的方法

--- test.air/everyday_task.py
+++ test.air/everyday_task.py
@@ -3,7 +3,7 @@ from airtest.core.api import *
 import logging
 from PIL import Image
 from init_airtest import AirConn
-from photos import Base,exists_ui,touch_ui,find_ui
+from photos import Base,exists_ui,touch_ui,find_ui,find_all_ui,shot,open_WXWork_window
 from airtest.aircv import *
 from airtest.core.api import Template, exists, touch, auto_setup, connect_device,snapshot
 
@@ -17,37 +17,85 @@ class EveryDayTask(Base):
     def find_the_chat(self):
         '''
         turn to the chat panel.
+        如果当前是消息页,就返回Trun
+        如果无法做操作就返回False
         '''
-        conn = AirConn(title='企业微信')
-        conn.connect_to_target_window()
-        if exists_ui('消息2'):
-            logger.info('点击消息按钮')
-            touch_ui('消息2')
+        if open_WXWork_window():
+            try:
+                self.connect_to_workwechat()
+                if exists_ui('消息1_1') and exists_ui('消息1'):
+                    self.connect_to_workwechat()
+                    logger.info('当前页面是消息页面')
+                    return True
+                elif exists_ui('消息2'):
+                    logger.info('点击消息按钮')
+                    touch_ui('消息2')
+                    self.connect_to_workwechat()
+                    logger.info('当前为消息列表页')
+                    return True
+            except Exception as e:
+                logger.error('进入消息列表页出错'+str(e))
+                return False
         else:
-            logger.info('当前页面是消息页面')
+            return False
 
     def search_the_SMR(self,str='洛书SMR-test'):
         '''
         search the str in chat-list.
         return Ture:已找到 False:未找到
         '''
-        conn = AirConn(title='企业微信')
-        conn.connect_to_target_window()
-        if exists_ui('搜索框'):
-            logger.info('点击放大镜')
-            touch_ui('搜索框放大镜')
-        if exists_ui('搜索框取消'):
-            logger.info('检测到搜索框中有文字,点击清空搜索框')
-            touch_ui('搜索框取消')
-        sleep(0.2)
-        # self.send_keys(str)
-        text(str)
-        if exists_ui('查询结果-洛书'):
-            logger.info('LuoShu-SMP mini program has been found and is ready to click on it')
-            touch_ui('查询结果-洛书')
-            return True
+        if self.connect_to_workwechat():
+            pass
         else:
-            logger.error('can not find the LuoShu-SMR mini-program')
+            self.find_the_chat()
+
+        if self.find_the_chat() and exists_ui('洛书SMR-test聊天窗口'):
+            '''
+            判断当前聊天页面是否是洛书SMR-test页面
+            如果是,直接返回True
+            不是继续往下走
+            '''
+            try:
+                self.connect_to_workwechat()
+                logger.info('鼠标点击聊天记录页面')
+                touch_ui('聊天页笑脸', y=-10)
+                return True
+            except Exception as e:
+                logger.error('搜索洛书SMR-test之后没有连接到企微窗口或没有有点击到聊天页面')
+                if self.find_the_chat():
+                    return True
+                else:
+                    return False
+        if self.find_the_chat():
+            '''
+            判断是否在消息列表页
+            如果在就进行搜索操作
+            '''
+            self.connect_to_workwechat()
+            if exists_ui('搜索框'):
+                logger.info('点击放大镜')
+                touch_ui('搜索框放大镜')
+            if exists_ui('搜索框取消'):
+                logger.info('检测到搜索框中有文字,点击清空搜索框')
+                touch_ui('搜索框取消')
+            sleep(0.2)
+            logger.info('input "洛书SMR-test"')
+            text(str)
+            if exists_ui('查询结果-洛书'):
+                logger.info('LuoShu-SMP mini program has been found and is ready to click on it')
+                touch_ui('查询结果-洛书')
+                try:
+                    self.connect_to_workwechat()
+                    logger.info('鼠标点击聊天记录页面')
+                    touch_ui('聊天页笑脸', y=-10)
+                    return True
+                except Exception as e:
+                    logger.error('搜索洛书SMR-test之后没有连接到企微窗口或没有有点击到聊天页面')
+                    return False
+            else:
+                logger.error('can not find the LuoShu-SMR mini-program')
+            return False
+        else:
             return False
 
     def back_to_latest_position(self):
@@ -57,26 +105,156 @@ class EveryDayTask(Base):
         if exists_ui('回到最新位置'):
             touch_ui('回到最新位置')
 
-    def click_message_list_task(self,coordinate):
+    def click_message_page(self):
         '''
-        Click to open the SOP box
-        :param coordinate:(x,y)坐标  -->The type is a tuple.
-        :return:
-        eg:点击窗口的x=100,y=100处位置
-            click_message_list_task(100,100)
+        Click and go to the message page
         '''
+        count = 0
+        while True:
+            touch('聊天页笑脸')
+            if exists_ui('') or exists_ui('') or exists_ui(''):
+                logger.info('检测到聊天页滑动控件')
+                break
+            else:
+                logger.info('没有检测到聊天页滑动控件')
+                count+=1
+            if count == 10:
+                pass
 
-        touch(coordinate)
-    def find_sop_task(self):
+    def is_in_chat_list(self):
         '''
-        Coordinates of all matched elements in the window
-        :param pic:a image path
+        确定当前在页面在洛书SMR-test对话框
+        如果不在,执行self.search_the_SMR
+        进入洛书SMR-test的聊天对话框并返回一个Trun
+        如果没有进入到洛书SMR聊天框,则返回False
         :return:
-        找到元素,返回每个元素的坐标 [(x1,y1),(x2,y2),...,(xn,yn)]
-        一直找到最顶上的消息都没有找到元素的坐标就返回 Fales
         '''
-        conn = AirConn(title='企业微信')
-        conn.connect_to_target_window()
+        while True:
+            if self.search_the_SMR():
+                try:
+                    self.connect_to_workwechat()
+                    touch_ui('聊天页笑脸', y=-10)
+                    return True
+                except Exception as e:
+                    logger.error('判断是否链接企微窗口或点击聊天页面的过程中出错'+str(e))
+                    return False
+            else:
+                return False
+
+    def up_to_find(self):
+        '''
+        Scroll up on the message page
+        如果两次截图的结果相等,则返回Turn 否则返回False
+        :return:
+        '''
+        if self.connect_to_workwechat():
+            pass
+        else:
+            self.find_the_chat()
+        if self.is_in_chat_list():
+            try:
+                logger.info('消息页置顶任务--开始做消息页置顶任务')
+                logger.info('消息页置顶任务--开始连接企微窗口')
+                self.connect_to_workwechat()
+            except Exception as e:
+                logger.error('消息页置顶任务--连接企微窗口失败'+str(e))
+                return False
+            try:
+                logger.info('消息页置顶任务--鼠标点击聊天记录页面')
+                touch_ui('聊天页笑脸', y=-10)
+            except Exception as e:
+                logger.error('消息页置顶任务--点击聊天页面出错'+str(e))
+                return False
+            while True:
+                logger.info('开始判断是否是消息列表最顶端')
+                logger.info('消息页置顶任务--开始连接企微窗口')
+                self.connect_to_workwechat()
+                logger.info('消息页置顶任务--鼠标点击聊天记录页面')
+                touch_ui('聊天页笑脸', y=-10)
+                try:
+                    logger.info('消息页置顶任务--准备在翻页前后截图做对比')
+                    snapshot(filename='test1.png')
+                    keyevent('{PGUP 5}')
+                    snapshot(filename='test2.png')
+                except Exception as e:
+                    logger.error('消息页置顶任务--执行截图或翻页时出错')
+                    return False
+                try:
+                    logger.debug('消息页置顶任务--开始比对前后截图')
+                    with open('log\\test1.png', 'rb') as test1:
+                        res1 = test1.read()
+                    with open('log\\test2.png', 'rb') as test2:
+                        res2 = test2.read()
+                except Exception as e:
+                    logger.error('消息页置顶任务--对比翻页前后截图出错')
+                    return False
+                if self.is_in_chat_list() and res1 ==res2:
+                    try:
+                        self.connect_to_workwechat()
+                        touch_ui('聊天页笑脸', y=-10)
+                        return True
+                    except Exception as e:
+                        logger.error('判断是否链接企微窗口或点击聊天页面的过程中出错')
+                        return False
+        else:
+            return False
+
+    def message_page_at_the_top(self,num=10):
+        '''
+        判断当前是否在消息的最顶端,如果是就返回Ture
+        num 传入一个int类型,表示判断当前页面就是置顶页面的次数
+        '''
+        if type(num) is int:
+            pass
+        else:
+            logger.error('传入的参数非int类型')
+            return False
+        flag_list = []#定义一个空列表用来存放最后对比的所有结果
+        c = True
+        if self.up_to_find():
+            while c:
+                for i in range(num):
+                    try:
+                        logger.info(f'重复确认{num}次是否是消息最顶端')
+                        logger.info('消息页置顶任务--开始连接企微窗口')
+                        self.connect_to_workwechat()
+                        logger.info('消息页置顶任务--鼠标点击聊天记录页面')
+                        touch_ui('聊天页笑脸', y=-10)
+                    except Exception as e:
+                        logger.info('企微窗口未找到,跳出for循环')
+                        break
+
+                    try:
+                        logger.info('消息页置顶任务--准备在翻页前后截图做对比')
+                        snapshot(filename='test1.png')
+                        keyevent('{PGUP}')
+                        snapshot(filename='test2.png')
+                        with open('log\\test1.png', 'rb') as test1:
+                            res1 = test1.read()
+                        with open('log\\test2.png', 'rb') as test2:
+                            res2 = test2.read()
+                            flag_list.append(res1 == res2)
+                    except Exception as e:
+                        logger.error('消息页置顶任务--确实当时是否为消息列表置顶时,截图对比出错'+str(e))
+                        return False
+
+
+                if all(flag_list) == True:
+                    try:
+
+                        logger.info('确认当前为最上边消息,并尝试连接企微窗口')
+                        self.connect_to_workwechat()
+                        logger.info('尝试用鼠标点击聊天记录页面')
+                        touch_ui('聊天页笑脸', y=-10)
+                        logger.info('确认当前企微页面没有被关闭,并且可以点击聊天页面,可以返回True')
+                        return self.is_in_chat_list()
+                    except Exception as e:
+                        logger.error('返回True之前乜有连接到企微窗口或没有点击到聊天页面'+str(e))
+                        return False
+                c = self.is_in_chat_list()
+        else:
+            return False
+
 
 
 
@@ -169,28 +347,31 @@ class EveryDayTask(Base):
         '''
         test
         '''
-        self.connect_to_select_custom_panel()
-        self.connect_to_desktop()
-        touch_ui('全选客户',x=-25)
+        # self.connect_to_select_custom_panel()
+        # self.connect_to_desktop()
+        # touch_ui('全选客户',x=-25)
+        self.connect_to_workwechat()
+        sleep(3)
+        shot('当前截屏')
+        image = aircv.imread(filename='F:\\git\\WorkWechat\\test.air\\photos\\当前截屏.png')
+        local = aircv.crop_image(image,(132,58,380,126))
+        show_origin_size(local)
+        touch(local)
+
 
     def run_task(self):
         '''
         '''
-        # self.find_the_chat()
-        # self.search_the_SMR()
-        # self.back_to_latest_position()
-        # self.receipt_the_custom_sop()
-        self.open_sending_helper()
-        self.select_the_customer()
-        self.select_customer_tag()
-        #self.connect_to_sop_chat()
-        a = self.find_sop_task()
+
+        # a = self.find_the_chat()  #--进入消息列表
+        # a = self.search_the_SMR()  #--搜索洛书SMR-test
+        # a = self.is_in_chat_list()  #判断是否在洛书SMR-test聊天页
+        a = self.up_to_find()  #判断是否在洛书SMR-test聊天页的最顶端
         print(a)
-        # touch((411, 149))
-        # a =  [(445, 378),(445, 195)]
-        # # b =
-        # print(a[0])
-        # self.click_message_list_task(a[0])
+
+
+        # print(self.message_page_at_the_top())
+        # self.test()
 
 
 
--- test.air/photos/base.py
+++ test.air/photos/base.py
@@ -1,6 +1,7 @@
 # -*- encoding=utf8 -*-
 __author__ = "26579"
 
+import winreg
 from abc import ABC
 import logging
 import time
@@ -12,10 +13,10 @@ from airtest.core.api import *
 from airtest.cli.parser import cli_setup
 
 from init_airtest import AirConn
-
-
+from psutil import process_iter
 
 logger = logging.getLogger(__name__)
+global hwnd_title
 
 
 class Base(ABC):
@@ -46,9 +47,12 @@ class Base(ABC):
         conn = AirConn(title='企业微信')
         try:
             conn.connect_to_target_window()
+            return True
         except Exception as e:
             logger.error(f'—— can not connect to the workwechat,detil error info: ——\n\t {e}')
-        sleep(0.2)
+            sleep(0.2)
+            return False
+
 
     def connect_to_sop_chat(self):
         '''
@@ -77,6 +81,17 @@ class Base(ABC):
         connect to the workwechat main window.
         '''
         conn = AirConn(title='选择客户')
+        try:
+            conn.connect_to_target_window()
+        except Exception as e:
+            logger.error(f'—— can not connect to the workwechat,detil error info: ——\n\t {e}')
+        sleep(0.2)
+
+    def connect_to_text_panel(self):
+        '''
+        connect to the workwechat main window.
+        '''
+        conn = AirConn(title='镜像源.txt - 记事本')
         try:
             conn.connect_to_target_window()
         except Exception as e:
@@ -126,7 +141,6 @@ def touch_ui(photo_name='',**kwargs):
             offset_y = 0 if kwargs.get('y') is None else kwargs.get('y')
             pos_x = int(pos[0].get('result')[0]) + offset_x
             pos_y = int(pos[0].get('result')[1]) + offset_y
-            print(pos_x,pos_y)
             touch((pos_x,pos_y))
     else:
         touch(Template('photos\%s.png' %photo_name))
@@ -141,14 +155,107 @@ def find_ui(photo_name=''):
     '''
     find all exists ui in panel.
     '''
-    return find_all(Template('photos\%s.png' %photo_name))
+    return find_all(Template(rf'photos\{photo_name}.png'))
+
 def find_all_ui(photo_name=''):
     '''
-    judge if the ui find_all.
-    :return 所有的匹配到的元素坐标-->list[(x1, y1),(x2, y2)...(xn, yn)]
+    find all exists ui in panel.
     '''
     list = []
-    result = find_all(Template('photos\%s.png' %photo_name))
-    for i in result:
+    res = find_all(Template(r'photos\%s.png' % photo_name))
+    for i in res:
         list.append(i.get('result'))
     return list
+
+def shot(photo_name=''):
+    snapshot(filename = '..\\photos\\%s.png' % photo_name,quality=99,max_size=1200)
+
+def get_WXWork_pid():
+    '''
+    Determine whether WXWork's process exists
+    If it doesn't exist,then return False
+    :return:
+    '''
+    try:
+        PID = process_iter()
+        for pid_temp in PID:
+            pid_dic = pid_temp.as_dict(attrs=['pid', 'name'])
+            if pid_dic['name'] == 'WXWork.exe':
+                pid_num = pid_dic['pid']
+                logger.info('找到企微 pid')
+                return pid_num
+            else:
+                logger.info('没有找到企微pid')
+                continue
+        logger.info('不存在企业微信pid')
+        return False
+    except Exception as e:
+        logger.error('获取pid时出错'+str(e))
+        return False
+
+def get_all_hwnd(hwnd, mouse):
+    if (win32gui.IsWindow(hwnd)
+            and win32gui.IsWindowEnabled(hwnd)
+            and win32gui.IsWindowVisible(hwnd)):
+        hwnd_title.update({hwnd: win32gui.GetWindowText(hwnd)})
+
+def get_WXWork_handle():
+    win32gui.EnumWindows(get_all_hwnd, 0)
+    for h, t in hwnd_title.items():
+        if t == '企业微信':
+            return True
+        else:
+            pass
+    return False
+
+def start_WXWork():
+    '''
+    通过链表找到企微的执行文件并启动
+    如果找到就启动它并返回True
+    否则就返回False
+    '''
+    try:
+        logger.info('获取企微执行文件路径')
+        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r'Software\Tencent\WXWork')  # 利用系统的链表
+        file_path = str(winreg.QueryValueEx(key, "Executable")[0])
+        logger.info('打开企业微信窗口')
+        app = pywinauto.Application(backend='uia')
+        app.start(file_path)
+        return True
+    except Exception as e:
+        logger.error('启动企微程序时出错'+str(e))
+        return False
+
+def open_WXWork_window():
+    '''
+    start WorkChat windows
+    if WXWork-process is killed
+    then return:False
+    else return:True
+    '''
+    global hwnd_title
+    hwnd_title = {}
+    if get_WXWork_pid() != False:
+        if get_WXWork_handle() == False:
+            try:
+                if start_WXWork():
+                    return True
+                else:
+                    return False
+            except Exception as e:
+                logger.error('获取企微执行文件路径过程中出错'+str(e))
+                return False
+        elif get_WXWork_handle() == True:
+            return True
+        else:
+            return False
+    else:
+        logger.info('企微没有运行,尝试启动企业微信')
+        if start_WXWork():
+            sleep(2)
+            return True
+        else:
+            return False
+
+if __name__ == '__main__':
+    print(open_WXWork_window())
